d466bf048f8f067e9dda0782e4fce5cb527c6074
Guillaume Alain <gyomalin@gmail.com>
2021-09-16 08:41:42 -0400

    name change for slurm_state

---
+++ slurm_state_test/anonymize_static_data.py
@@ -0,0 +1,9 @@
+"""
+It's nice to use real data to populate our tests, but let's not
+actually use information that is considered private for that.
+
+We will anonymize it by obfuscating a lot of the fields,
+starting with anything containing a potential user name.
+"""
+
+# TODO : Implement this.
