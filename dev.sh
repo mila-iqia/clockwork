63271925ce5dc0c541caa66db07582c63546b3f7
Arnaud Bergeron <abergeron@gmail.com>
2021-11-09 13:58:01 -0500

    Fix some problems

--- dev.sh
+++ dev.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+set -eu
+
 docker build -t clockwork_web clockwork_web
 docker build -t clockwork_web_test clockwork_web_test
-docker build -t clockwork_tools_test clockwork_tools_test
+docker build -t clockwork_tools_test -f clockwork_tools_test/Dockerfile .
 
 . ./env.sh
 
-export clockwork_tools_test_HOST="clockwork_dev"
+export clockwork_tools_test_HOST="localhost"
 docker-compose -f setup_ecosystem/docker-compose.yml run --service-ports clockwork_dev
