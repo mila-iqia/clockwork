b67b9df654a9a054cb8a4bc1fa241496b9129216
Guillaume Alain <gyomalin@gmail.com>
2021-11-16 15:40:51 -0500

    created a cmdline script

--- slurm_state/mongo_update3.py
+++ slurm_state/mongo_update3.py
@@ -27,23 +27,13 @@ def _fetch_slurm_report_helper(parser, cluster_desc_path, scontrol_report_path):
         scontrol_report_path
     ), f"scontrol_report_path {scontrol_report_path} is missing."
 
-    class CTX:
-        pass
-
     with open(cluster_desc_path, "r") as f:
-        E = json.load(f)
-        ctx_beluga = CTX()
-        for (k, v) in E.items():
-            if k in ["timezone"]:
-                pass
-            setattr(ctx_beluga, k, v)
-        setattr(ctx_beluga, "timezone", zoneinfo.ZoneInfo(E["timezone"]))
+        ctx_beluga = json.load(f)
+        ctx_beluga["timezone"] = zoneinfo.ZoneInfo(ctx_beluga["timezone"])
 
     with open(scontrol_report_path, "r") as f:
         for e in parser(f, ctx_beluga):
-            # hardcode that one because it's not being added at the moment
-            # in the functions from scontrol_parser.py (TODO : ask Arnaud about it)
-            e["cluster_name"] = ctx_beluga.name
+            e["cluster_name"] = ctx_beluga["name"]
             yield e
 
 
@@ -80,13 +70,13 @@ def infer_user_accounts(clockwork_job: dict[dict]):
             clockwork_job["cw"][k] = clockwork_job["slurm"][k]
     return clockwork_job
 
-def none_string_to_none_object(D:dict):
-    """
-    Returns a new dict with the same content
-    as the `D` argument, but whenever a key is '(null)'
-    it is turned into a `None` object instead.
-    """
-    return dict( ((k, None) if v == '(null)' else (k, v)) for (k, v) in D.items() )
+# def none_string_to_none_object(D:dict):
+#     """
+#     Returns a new dict with the same content
+#     as the `D` argument, but whenever a key is '(null)'
+#     it is turned into a `None` object instead.
+#     """
+#     return dict( ((k, None) if v == '(null)' else (k, v)) for (k, v) in D.items() )
 
 
 def slurm_node_to_clockwork_node(slurm_node: dict):
@@ -105,16 +95,10 @@ def slurm_node_to_clockwork_node(slurm_node: dict):
     return clockwork_node
 
 
-def main_read_jobs_and_update_collection(jobs_collection):
-
-    # https://stackoverflow.com/questions/33541290/how-can-i-create-an-index-with-pymongo
-    # Apparently "ensure_index" is deprecated, and we should always call "create_index".
-    timestamp_start = time.time()
-    jobs_collection.create_index(
-        [("slurm.job_id", 1), ("slurm.cluster_name", 1)], name="job_id_and_cluster_name"
-    )
-    create_index_duration = time.time() - timestamp_start
-    print(f"jobs_collection.create_index took {create_index_duration} seconds.")
+def main_read_jobs_and_update_collection(
+    jobs_collection,
+    cluster_desc_path,
+    scontrol_show_job_path):
 
     # What we want is to create the entry as
     #    {"slurm": slurm_dict, "cw": cw_dict, "user": user_dict}
@@ -125,82 +109,68 @@ def main_read_jobs_and_update_collection(jobs_collection):
     # Note that adding an extra {"$set": {"cw.last_slurm_update": time.time()}}
     # after the $setOnInsert operator does not work with mongodb.
 
-    for (cluster_desc_path, scontrol_show_job_path) in [
-        ("./cluster_desc/mila.json", "../tmp/slurm_report/mila/scontrol_show_job"),
-        ("./cluster_desc/beluga.json", "../tmp/slurm_report/beluga/scontrol_show_job"),
-        ("./cluster_desc/cedar.json", "../tmp/slurm_report/cedar/scontrol_show_job"),
-        ("./cluster_desc/graham.json", "../tmp/slurm_report/graham/scontrol_show_job"),
-    ]:
+    timestamp_start = time.time()
+    L_updates_to_do = []
 
-        timestamp_start = time.time()
-        L_updates_to_do = []
-
-        for (n, D_job) in enumerate(
+    for (n, D_job) in enumerate(
+        map(
+            infer_user_accounts,
             map(
-                infer_user_accounts,
-                map(
-                    slurm_job_to_clockwork_job,
-                    map(none_string_to_none_object,
-                        fetch_slurm_report_jobs(cluster_desc_path, scontrol_show_job_path)),
-                ),
-            )
-        ):
-            # if 4 <= n:
-            #    break
-            if n < 2:
-                print(D_job)
+                slurm_job_to_clockwork_job,
+                    fetch_slurm_report_jobs(cluster_desc_path, scontrol_show_job_path)
+            ),
+        )
+    ):
+        # if 4 <= n:
+        #    break
+        if n < 2:
+            print(D_job)
 
-            L_updates_to_do.append(
-                UpdateOne(
-                    # rule to match if already present in collection
-                    {
-                        "slurm.job_id": D_job["slurm"]["job_id"],
-                        "slurm.cluster_name": D_job["slurm"]["cluster_name"],
-                    },
-                    # the data that we write in the collection
-                    {
-                        "$set": {"slurm": D_job["slurm"]},
-                        "$setOnInsert": {"cw": D_job["cw"], "user": D_job["user"]},
-                    },
-                    # create if missing, update if present
-                    upsert=True,
-                )
+        L_updates_to_do.append(
+            UpdateOne(
+                # rule to match if already present in collection
+                {
+                    "slurm.job_id": D_job["slurm"]["job_id"],
+                    "slurm.cluster_name": D_job["slurm"]["cluster_name"],
+                },
+                # the data that we write in the collection
+                {
+                    "$set": {"slurm": D_job["slurm"]},
+                    "$setOnInsert": {"cw": D_job["cw"], "user": D_job["user"]},
+                },
+                # create if missing, update if present
+                upsert=True,
             )
-
-            # Here we can add set extra values to "cw".
-            L_updates_to_do.append(
-                UpdateOne(
-                    # rule to match if already present in collection
-                    {
-                        "slurm.job_id": D_job["slurm"]["job_id"],
-                        "slurm.cluster_name": D_job["slurm"]["cluster_name"],
-                    },
-                    # the data that we write in the collection
-                    {"$set": {"cw.last_slurm_update": time.time()}},
-                    # create if missing, update if present
-                    upsert=False,
-                )
-            )
-
-        result = jobs_collection.bulk_write(L_updates_to_do)  #  <- the actual work
-        # print(result.bulk_api_result)
-        mongo_update_duration = time.time() - timestamp_start
-        print(
-            f"Bulk write for {len(L_updates_to_do)} job entries in mongodb took {mongo_update_duration} seconds."
         )
 
+        # Here we can add set extra values to "cw".
+        L_updates_to_do.append(
+            UpdateOne(
+                # rule to match if already present in collection
+                {
+                    "slurm.job_id": D_job["slurm"]["job_id"],
+                    "slurm.cluster_name": D_job["slurm"]["cluster_name"],
+                },
+                # the data that we write in the collection
+                {"$set": {"cw.last_slurm_update": time.time()}},
+                # create if missing, update if present
+                upsert=False,
+            )
+        )
 
-
-def main_read_nodes_and_update_collection(nodes_collection):
-
-    # https://stackoverflow.com/questions/33541290/how-can-i-create-an-index-with-pymongo
-    # Apparently "ensure_index" is deprecated, and we should always call "create_index".
-    timestamp_start = time.time()
-    nodes_collection.create_index(
-        [("slurm.name", 1), ("slurm.cluster_name", 1)], name="name_and_cluster_name"
+    result = jobs_collection.bulk_write(L_updates_to_do)  #  <- the actual work
+    # print(result.bulk_api_result)
+    mongo_update_duration = time.time() - timestamp_start
+    print(
+        f"Bulk write for {len(L_updates_to_do)} job entries in mongodb took {mongo_update_duration} seconds."
     )
-    create_index_duration = time.time() - timestamp_start
-    print(f"nodes_collection.create_index took {create_index_duration} seconds.")
+
+
+
+def main_read_nodes_and_update_collection(
+    nodes_collection,
+    cluster_desc_path,
+    scontrol_show_node_path):
 
     # What we want is to create the entry as
     #    {"slurm": slurm_dict, "cw": cw_dict}
@@ -208,68 +178,66 @@ def main_read_nodes_and_update_collection(nodes_collection):
     # but if it actually is already there then we only want
     # to update the "slurm" part of it.
 
-    for (cluster_desc_path, scontrol_show_node_path) in [
-        ("./cluster_desc/mila.json", "../tmp/slurm_report/mila/scontrol_show_node"),
-        ("./cluster_desc/beluga.json", "../tmp/slurm_report/beluga/scontrol_show_node"),
-        ("./cluster_desc/cedar.json", "../tmp/slurm_report/cedar/scontrol_show_node"),
-        ("./cluster_desc/graham.json", "../tmp/slurm_report/graham/scontrol_show_node"),
-    ]:
+    timestamp_start = time.time()
+    L_updates_to_do = []
 
-        timestamp_start = time.time()
-        L_updates_to_do = []
+    for (n, D_node) in enumerate(
+        map(
+            slurm_node_to_clockwork_node,
+                fetch_slurm_report_nodes(cluster_desc_path, scontrol_show_node_path)
+        ),
+    ):
+        # if 4 <= n:
+        #    break
+        if n < 2:
+            print(D_node)
 
-        for (n, D_node) in enumerate(
-            map(
-                slurm_node_to_clockwork_node,
-                map(none_string_to_none_object,
-                    fetch_slurm_report_nodes(cluster_desc_path, scontrol_show_node_path)),
-            ),
-        ):
-            # if 4 <= n:
-            #    break
-            if n < 2:
-                print(D_node)
-
-            L_updates_to_do.append(
-                UpdateOne(
-                    # rule to match if already present in collection
-                    {
-                        "slurm.name": D_node["slurm"]["name"],
-                        "slurm.cluster_name": D_node["slurm"]["cluster_name"],
-                    },
-                    # the data that we write in the collection
-                    {
-                        "$set": {"slurm": D_node["slurm"]},
-                        "$setOnInsert": {"cw": D_node["cw"]},
-                    },
-                    # create if missing, update if present
-                    upsert=True,
-                )
+        L_updates_to_do.append(
+            UpdateOne(
+                # rule to match if already present in collection
+                {
+                    "slurm.name": D_node["slurm"]["name"],
+                    "slurm.cluster_name": D_node["slurm"]["cluster_name"],
+                },
+                # the data that we write in the collection
+                {
+                    "$set": {"slurm": D_node["slurm"]},
+                    "$setOnInsert": {"cw": D_node["cw"]},
+                },
+                # create if missing, update if present
+                upsert=True,
             )
-
-            # Here we can add set extra values to "cw".
-            L_updates_to_do.append(
-                UpdateOne(
-                    # rule to match if already present in collection
-                    {
-                        "slurm.name": D_node["slurm"]["name"],
-                        "slurm.cluster_name": D_node["slurm"]["cluster_name"],
-                    },
-                    # the data that we write in the collection
-                    {"$set": {"cw.last_slurm_update": time.time()}},
-                    # create if missing, update if present
-                    upsert=False,
-                )
-            )
-
-        result = nodes_collection.bulk_write(L_updates_to_do)  #  <- the actual work
-        # print(result.bulk_api_result)
-        mongo_update_duration = time.time() - timestamp_start
-        print(
-            f"Bulk write for {len(L_updates_to_do)} node entries in mongodb took {mongo_update_duration} seconds."
         )
 
+        # Here we can add set extra values to "cw".
+        L_updates_to_do.append(
+            UpdateOne(
+                # rule to match if already present in collection
+                {
+                    "slurm.name": D_node["slurm"]["name"],
+                    "slurm.cluster_name": D_node["slurm"]["cluster_name"],
+                },
+                # the data that we write in the collection
+                {"$set": {"cw.last_slurm_update": time.time()}},
+                # create if missing, update if present
+                upsert=False,
+            )
+        )
+
+    result = nodes_collection.bulk_write(L_updates_to_do)  #  <- the actual work
+    # print(result.bulk_api_result)
+    mongo_update_duration = time.time() - timestamp_start
+    print(
+        f"Bulk write for {len(L_updates_to_do)} node entries in mongodb took {mongo_update_duration} seconds."
+    )
+
+
 def run():
+    """
+    This function is never going to be called in production.
+    It's there mostly to test this script without going through
+    "read_report_commit_to_db.py".
+    """
 
     connection_string = os.environ.get("MONGODB_CONNECTION_STRING", "")
     assert connection_string
@@ -278,8 +246,43 @@ def run():
     database_name = os.environ.get("MONGODB_DATABASE_NAME", "")
     assert database_name
 
-    # main_read_jobs_and_update_collection(client[database_name]["jobs"])
-    main_read_nodes_and_update_collection(client[database_name]["nodes"])
+
+    # https://stackoverflow.com/questions/33541290/how-can-i-create-an-index-with-pymongo
+    # Apparently "ensure_index" is deprecated, and we should always call "create_index".
+    timestamp_start = time.time()
+    client[database_name]["nodes"].create_index(
+        [("slurm.name", 1), ("slurm.cluster_name", 1)], name="name_and_cluster_name"
+    )
+    client[database_name]["jobs"].create_index(
+        [("slurm.job_id", 1), ("slurm.cluster_name", 1)], name="job_id_and_cluster_name"
+    )
+    create_index_duration = time.time() - timestamp_start
+    print(f"create_index took {create_index_duration} seconds.")
+
+
+    # Now let's do both nodes and jobs, with hardcoded paths.
+
+    for (cluster_desc_path, scontrol_show_node_path) in [
+        ("./cluster_desc/mila.json", "../tmp/slurm_report/mila/scontrol_show_node"),
+        ("./cluster_desc/beluga.json", "../tmp/slurm_report/beluga/scontrol_show_node"),
+        ("./cluster_desc/cedar.json", "../tmp/slurm_report/cedar/scontrol_show_node"),
+        ("./cluster_desc/graham.json", "../tmp/slurm_report/graham/scontrol_show_node"),
+    ]:
+        main_read_nodes_and_update_collection(
+            client[database_name]["nodes"],
+            cluster_desc_path, scontrol_show_node_path)
+
+    for (cluster_desc_path, scontrol_show_job_path) in [
+        ("./cluster_desc/mila.json", "../tmp/slurm_report/mila/scontrol_show_job"),
+        ("./cluster_desc/beluga.json", "../tmp/slurm_report/beluga/scontrol_show_job"),
+        # ("./cluster_desc/cedar.json", "../tmp/slurm_report/cedar/scontrol_show_job"),
+        ("./cluster_desc/graham.json", "../tmp/slurm_report/graham/scontrol_show_job"),
+    ]:
+        main_read_jobs_and_update_collection(
+            client[database_name]["jobs"],
+            cluster_desc_path, scontrol_show_job_path)
+
+
 
 
 if __name__ == "__main__":
