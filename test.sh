20e3e2c00015a280051d23325e70a4f042e86327
Arnaud Bergeron <abergeron@gmail.com>
2021-11-18 18:17:45 -0500

    Package the slurm_state tests in docker too

--- test.sh
+++ test.sh
@@ -4,6 +4,7 @@ set -eu
 docker build -t clockwork_web clockwork_web
 docker build -t clockwork_web_test clockwork_web_test
 docker build -t clockwork_tools_test -f clockwork_tools_test/Dockerfile .
+docker build -t slurm_state_test -f slurm_state_test/Dockerfile .
 
 . ./env.sh
 
@@ -12,5 +13,7 @@ trap "docker-compose -f setup_ecosystem/docker-compose.yml down && docker-compos
 
 docker-compose -f setup_ecosystem/docker-compose.yml run clockwork_web_test
 docker-compose -f setup_ecosystem/docker-compose.yml run clockwork_tools_test
+docker-compose -f setup_ecosystem/docker-compose.yml run slurm_state_test
 
-
+coverage combine clockwork_web_test/.coverage clockwork_tools_test/.coverage slurm_state_test/.coverage
+coverage report -m
