e1ae64d350a370152b7effc50f486709ea17d84c
Guillaume Alain <gyomalin@gmail.com>
2021-11-24 18:49:39 -0500

    generating fake data, tests now passing

--- slurm_state/anonymize_scontrol_report.py
+++ slurm_state/anonymize_scontrol_report.py
@@ -19,18 +19,48 @@ import argparse
 import numpy as np
 
 
-def get_account_name():
-    return np.random.choice(
-        ["def-patate-rrg", "def-pomme-rrg", "def-cerise-rrg", "def-citron-rrg"]
-    )
+def get_predefined_fake_users():
+
+    M = 10
+
+    LD_users = []
+    n = 0
+    for _ in range(M):
+        for account in [
+            "def-patate-rrg",
+            "def-pomme-rrg",
+            "def-cerise-rrg",
+            "def-citron-rrg",
+        ]:
+            for cluster_name in ["beluga", "graham", "cedar", "narval"]:
+                n = n + 1
+                LD_users.append(
+                    {
+                        "username": "ccuser%0.2d" % n,
+                        "uid": "%d" % (10000 + n),
+                        "account": account,
+                        "cluster_name": cluster_name,
+                    }
+                )
+
+    for _ in range(M):
+        for __ in range(4):  # compensate for the other one getting 4x accounts
+            account = "mila"
+            cluster_name = "mila"
+            n = n + 1
+            LD_users.append(
+                {
+                    "username": "milauser%0.2d" % n,
+                    "uid": "%d" % (10000 + n),
+                    "account": "mila",
+                    "cluster_name": "mila",
+                }
+            )
+    return LD_users
 
 
 def get_random_name():
     return "somename_%d" % np.random.randint(low=0, high=1e6)
-
-
-def get_random_username():
-    return np.random.choice(["mario", "luigi", "toad", "peach"])
 
 
 # def get_random_node_name():
@@ -45,7 +75,7 @@ def get_random_path():
     )
 
 
-def process_line(line):
+def process_line(line, D_user):
     """
     Anonymize the lines from the report.
     This involves doing a hatchet job to some fields
@@ -63,17 +93,15 @@ def process_line(line):
             get_random_name(),
         )
     if m := re.match(r"(\s*)(UserId=.+?)\s(GroupId=.+?)\s(MCS_label=.+?)\s*", line):
-        u = get_random_username()
-        d = np.random.randint(low=0, high=1e6)
-        return "%sUserId=%s(%d) GroupId=%s(%d) MCS_label=N/A\n" % (
+        return "%sUserId=%s(%s) GroupId=%s(%s) MCS_label=N/A\n" % (
             m.group(1),
-            u,
-            d,
-            u,
-            d,
+            D_user["username"],
+            D_user["uid"],
+            D_user["username"],
+            D_user["uid"],
         )
     if m := re.match(r"(.+)(Account=.+?)\s(.+)", line):
-        return "%sAccount=%s %s\n" % (m.group(1), get_account_name(), m.group(3))
+        return "%sAccount=%s %s\n" % (m.group(1), D_user["account"], m.group(3))
     if m := re.match(r"(\s*)(Command=.+?)\s*", line):
         return "%sCommand=%s\n" % (m.group(1), get_random_path())
     if m := re.match(r"(\s*)(WorkDir=.+?)\s*", line):
@@ -111,6 +139,8 @@ def process_line(line):
         )
     if m := re.match(r"(\s*)Partitions=", line):
         return "%sPartitions=fun_partitions \n" % (m.group(1),)
+    if m := re.match(r"(\s*)Reason=.*", line):
+        return "%sReason=partying \n" % (m.group(1),)
 
     return line
 
@@ -121,16 +151,43 @@ def main(argv):
         description="Take a report from scontrol and strip out all identifying information.",
     )
 
+    parser.add_argument(
+        "-c",
+        "--cluster_name",
+        help="The cluster name. Helps with generating plausible users.",
+    )
     parser.add_argument("-i", "--input_file", help="The jobs or nodes report file.")
     parser.add_argument("-o", "--output_file", help="Output file.")
+    parser.add_argument(
+        "-k",
+        "--keep",
+        type=int,
+        default=None,
+        help="Number of values to keep. Ignore the rest.",
+    )
 
     args = parser.parse_args(argv[1:])
     print(args)
 
+    LD_users = get_predefined_fake_users()
+    # make sure the cluster_name given is found in at least one user
+    LD_users_on_that_cluster = [
+        D_user for D_user in LD_users if D_user["cluster_name"] == args.cluster_name
+    ]
+    assert len(LD_users_on_that_cluster)
+    D_user = None
+    nbr_processed = 0
+
     with open(args.input_file, "r") as f_in:
         with open(args.output_file, "w") as f_out:
             for line in f_in.readlines():
-                f_out.write(process_line(line))
+                # pick a new user every time we hit an empty line
+                if D_user is None or re.match(r"^\s*$", line):
+                    D_user = np.random.choice(LD_users_on_that_cluster)
+                    nbr_processed = nbr_processed + 1
+                    if args.keep and (args.keep <= nbr_processed - 1):
+                        quit()
+                f_out.write(process_line(line, D_user))
 
 
 if __name__ == "__main__":
@@ -140,12 +197,16 @@ if __name__ == "__main__":
 
 """
 python3 anonymize_scontrol_report.py \
+    --cluster_name beluga \
     --input_file ../tmp/slurm_report/beluga/scontrol_show_job \
     --output_file ../tmp/slurm_report/beluga/scontrol_show_job_anonymized
 
 python3 anonymize_scontrol_report.py \
+    --cluster_name beluga \
     --input_file ../tmp/slurm_report/beluga/scontrol_show_node \
     --output_file ../tmp/slurm_report/beluga/scontrol_show_node_anonymized
+
+
 
 
 """
