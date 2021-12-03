d4166556beedde10ea9022dd3bdc92a267ae48c7
Arnaud Bergeron <abergeron@gmail.com>
2021-12-03 16:38:35 -0500

    Remove the setup_ecosystem directory

--- test.sh
+++ test.sh
@@ -9,8 +9,8 @@ docker build -t slurm_state_test -f slurm_state_test/Dockerfile .
 . ./env.sh
 
 # This is to ensure that there aren't lingering containers after the test script exits.
-trap "docker-compose -f setup_ecosystem/docker-compose.yml down && docker-compose -f setup_ecosystem/docker-compose.yml rm -fv" EXIT
+trap "docker-compose down && docker-compose rm -fv" EXIT
 
-docker-compose -f setup_ecosystem/docker-compose.yml run clockwork_web_test
-docker-compose -f setup_ecosystem/docker-compose.yml run clockwork_tools_test
-docker-compose -f setup_ecosystem/docker-compose.yml run slurm_state_test
+docker-compose run clockwork_web_test
+docker-compose run clockwork_tools_test
+docker-compose run slurm_state_test
