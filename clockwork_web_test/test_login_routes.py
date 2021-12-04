22398c59eeae909a5c21d8573910044d22332e3f
Arnaud Bergeron <abergeron@gmail.com>
2021-12-03 19:05:18 -0500

    Add better matching in the fake responses

--- clockwork_web_test/test_login_routes.py
+++ clockwork_web_test/test_login_routes.py
@@ -2,41 +2,45 @@ from urllib.parse import urlparse, parse_qs
 from clockwork_web.login_routes import GOOGLE_DISCOVERY_URL
 
 
-def test_login(client_with_login, requests_mock):
-    requests_mock.get(
+def fake_endpoints(mock, userinfo_text):
+    mock.get(
         GOOGLE_DISCOVERY_URL,
         text="""{
-            "authorization_endpoint": "https://localhost:8080/authorize",
-            "token_endpoint": "https://localhost:8080/token",
-            "userinfo_endpoint": "https://localhost:8080/userinfo"
-    }""",
+        "authorization_endpoint": "https://localhost:8080/authorize",
+        "token_endpoint": "https://localhost:8080/token",
+        "userinfo_endpoint": "https://localhost:8080/userinfo"
+        }""",
     )
+    mock.register_uri(
+        "POST",
+        "https://localhost:8080/token",
+        additional_matcher=lambda req: req.body
+        == "grant_type=authorization_code&client_id=&code=patate32&redirect_uri=https%3A%2F%2Flocalhost%2Flogin%2Fcallback",
+        text='{"access_token": "this_is_access_token"}',
+    )
+    mock.get(
+        "https://localhost:8080/userinfo",
+        request_headers={"Authorization": "Bearer this_is_access_token"},
+        text=userinfo_text,
+    )
+
+
+def test_login(client_with_login, requests_mock):
+    fake_endpoints(requests_mock, "")
     resp = client_with_login.get("/login/")
     assert resp.status_code == 302
     assert resp.headers["Location"].startswith(
-        "https://localhost:8080/authorize?response_type=code&client_id=None&redirect_uri=https%3A%2F%2Flocalhost%2Flogin%2Fcallback&scope=openid+email+profile&state="
+        "https://localhost:8080/authorize?response_type=code&client_id=&redirect_uri=https%3A%2F%2Flocalhost%2Flogin%2Fcallback&scope=openid+email+profile&state="
     )
 
 
 def test_login_noemail(client_with_login, requests_mock):
-    requests_mock.get(
-        GOOGLE_DISCOVERY_URL,
-        text="""{
-            "authorization_endpoint": "https://localhost:8080/authorize",
-            "token_endpoint": "https://localhost:8080/token",
-            "userinfo_endpoint": "https://localhost:8080/userinfo"
-    }""",
-    )
+    fake_endpoints(requests_mock, "{}")
     resp = client_with_login.get("/login/")
     res = urlparse(resp.headers["Location"])
     args = parse_qs(res.query)
     assert args["redirect_uri"][0] == "https://localhost/login/callback"
     code = "patate32"
-    # This fill fake the response from requests in the login code
-    requests_mock.post(
-        "https://localhost:8080/token", text='{"access_token": "this_is_access_token"}'
-    )
-    requests_mock.get("https://localhost:8080/userinfo", text="{}")
 
     resp = client_with_login.get(
         "/login/callback", query_string=dict(code=code, state=args["state"][0])
@@ -46,28 +50,15 @@ def test_login_noemail(client_with_login, requests_mock):
 
 
 def test_login_nomila(client_with_login, requests_mock):
-    requests_mock.get(
-        GOOGLE_DISCOVERY_URL,
-        text="""{
-            "authorization_endpoint": "https://localhost:8080/authorize",
-            "token_endpoint": "https://localhost:8080/token",
-            "userinfo_endpoint": "https://localhost:8080/userinfo"
-    }""",
+    fake_endpoints(
+        requests_mock,
+        '{"email_verified": true, "sub": 1234, "email": "random@gmail.com", "picture": "", "given_name": "Random"}',
     )
     resp = client_with_login.get("/login/")
     res = urlparse(resp.headers["Location"])
     args = parse_qs(res.query)
     assert args["redirect_uri"][0] == "https://localhost/login/callback"
     code = "patate32"
-    # This fill fake the response from requests in the login code
-    requests_mock.post(
-        "https://localhost:8080/token", text='{"access_token": "this_is_access_token"}'
-    )
-    requests_mock.get(
-        "https://localhost:8080/userinfo",
-        text='{"email_verified": true, "sub": 1234, "email": "random@gmail.com", "picture": "", "given_name": "Random"}',
-    )
-
     resp = client_with_login.get(
         "/login/callback", query_string=dict(code=code, state=args["state"][0])
     )
@@ -75,29 +66,16 @@ def test_login_nomila(client_with_login, requests_mock):
     assert b"We accept only accounts @mila.quebec" in resp.data
 
 
-def Xtest_login_new(client_with_login, requests_mock):
-    requests_mock.get(
-        GOOGLE_DISCOVERY_URL,
-        text="""{
-            "authorization_endpoint": "https://localhost:8080/authorize",
-            "token_endpoint": "https://localhost:8080/token",
-            "userinfo_endpoint": "https://localhost:8080/userinfo"
-    }""",
+def test_login_new(client_with_login, requests_mock):
+    fake_endpoints(
+        requests_mock,
+        '{"email_verified": true, "sub": 1234, "email": "user@mila.quebec", "picture": "", "given_name": "User"}',
     )
     resp = client_with_login.get("/login/")
     res = urlparse(resp.headers["Location"])
     args = parse_qs(res.query)
     assert args["redirect_uri"][0] == "https://localhost/login/callback"
     code = "patate32"
-    # This fill fake the response from requests in the login code
-    requests_mock.post(
-        "https://localhost:8080/token", text='{"access_token": "this_is_access_token"}'
-    )
-    requests_mock.get(
-        "https://localhost:8080/userinfo",
-        text='{"email_verified": true, "sub": 1234, "email": "user@mila.quebec", "picture": "", "given_name": "Random"}',
-    )
-
     resp = client_with_login.get(
         "/login/callback", query_string=dict(code=code, state=args["state"][0])
     )
@@ -107,4 +85,4 @@ def Xtest_login_new(client_with_login, requests_mock):
 
     resp = client_with_login.get("/")
     assert resp.status_code == 200
-    assert "Not logged in" not in resp.data
+    assert b"Not logged in" not in resp.data
