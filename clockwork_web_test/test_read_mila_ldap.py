0687163129f2d1f539c0d3a701383fef837e0fcc
Guillaume Alain <gyomalin@gmail.com>
2022-02-03 15:11:56 -0500

    all tests passing

--- clockwork_web_test/test_read_mila_ldap.py
+++ clockwork_web_test/test_read_mila_ldap.py
@@ -4,10 +4,8 @@ import pytest
 from clockwork_web.db import get_db, init_db
 from clockwork_web.scripts import read_mila_ldap
 
-import os
-import tempfile
-import json
-
+import string
+import random
 
 def test_database_update_users(app):
     """
@@ -17,7 +15,7 @@ def test_database_update_users(app):
 
     LD_users = [
         {
-            "mila_email_username": "johnsmith",
+            "mila_email_username": "johnsmith@mila.quebec",
             "mila_cluster_username": "johnsmith",
             "mila_cluster_uid": "1500000001",
             "mila_cluster_gid": "1500000001",
@@ -25,7 +23,7 @@ def test_database_update_users(app):
             "status": "enabled",
         },
         {
-            "mila_email_username": "grouchomarx",
+            "mila_email_username": "grouchomarx@mila.quebec",
             "mila_cluster_username": "grouchomarx",
             "mila_cluster_uid": "1500000002",
             "mila_cluster_gid": "1500000002",
@@ -36,7 +34,7 @@ def test_database_update_users(app):
 
     LD_users_updates = [
         {
-            "mila_email_username": "johnsmith",
+            "mila_email_username": "johnsmith@mila.quebec",
             "mila_cluster_username": "johnsmith",
             "mila_cluster_uid": "1500000001",
             "mila_cluster_gid": "1500000001",
@@ -44,7 +42,7 @@ def test_database_update_users(app):
             "status": "disabled",  # this will change in the database
         },
         {
-            "mila_email_username": "grouchomarx",
+            "mila_email_username": "grouchomarx@mila.quebec",
             "mila_cluster_username": "grouchomarx",
             "mila_cluster_uid": "1500000002",
             "mila_cluster_gid": "1500000002",
@@ -77,8 +75,6 @@ def test_database_update_users(app):
             LD_users=LD_users,
         )
 
-        print(list(mc[collection_name].find()))
-
         for D_user in LD_users:
 
             LD_found_user = list(mc[collection_name].find(
@@ -103,8 +99,8 @@ def test_database_update_users(app):
                 {"mila_email_username": D_user["mila_email_username"]},
                 {
                     "$set": {
-                        "cc_account_username": ("cc_" + D_user["mila_email_username"]),
-                        "clockwork_web_api": "0300whatever813817"
+                        "cc_account_username": ("cc_" + D_user["mila_email_username"].split("@")[0]),
+                        "clockwork_api_key": "0300whatever813817"
                     }
                 })
 
@@ -124,7 +120,7 @@ def test_database_update_users(app):
                     # This is something specific that we test.
                     # They should both end up being "disabled".
                     assert D_found_user[k] == "disabled"
-                elif k in ["cc_account_username", "clockwork_web_api"]:
+                elif k in ["cc_account_username", "clockwork_api_key"]:
                     assert D_found_user[k] is not None
                 else:
                     assert D_found_user[k] == D_user[k]
@@ -133,3 +129,74 @@ def test_database_update_users(app):
         # clear out what you put in the db for this test
         mc[collection_name].delete_many({})
 
+
+
+
+
+def test_client_side_user_updates():
+    """
+    This tests the function client_side_user_updates
+    that has all the logic rules for the updates.
+    This is indirectly tested also by `test_database_update_users`
+    but we'll test it with more data here.
+    """
+
+    N = 100
+    LD_users_DB = [
+        {
+            "mila_email_username": name + "@mila.quebec",
+            "mila_cluster_username": name + "@mila.quebec",
+            "mila_cluster_uid": "%d" % (1500000000 + i),
+            "mila_cluster_gid": "%d" % (1500000000 + i),
+            "display_name": name.upper(),  # whatever
+            "status": "enabled" if random.random() > 0.5 else "disabled",
+            "clockwork_api_key": None if random.random() > 0.5 else str(random.random()),
+            "cc_account_username": None if random.random() > 0.5 else ("cc_" + name),
+        } for (i, name) in enumerate(
+            "".join(
+                random.choices(string.ascii_lowercase + string.ascii_uppercase + string.digits, k=20)
+            ) for _ in range(N))
+    ]
+
+    LD_users_LDAP = []
+    for D_user_DB in LD_users_DB:
+        # copy it first
+        D_user_LDAP = dict( e for e in D_user_DB.items() )
+        # those two fields will never be in LDAP updates
+        del D_user_LDAP["clockwork_api_key"]
+        del D_user_LDAP["cc_account_username"]
+        D_user_LDAP["status"] = "enabled" if random.random() > 0.5 else "disabled"
+        LD_users_LDAP.append(D_user_LDAP)
+
+    # Then, because we want to have new users in the updates,
+    # we'll just drop half of them from D_user_DB.
+    random.shuffle(LD_users_DB)
+    LD_users_DB = LD_users_DB[N//2:]
+
+
+    LD_users_updated = read_mila_ldap.client_side_user_updates(
+        LD_users_DB=LD_users_DB, LD_users_LDAP=LD_users_LDAP)
+
+    # Again the trick to use dict to avoid N^2 perf.
+    DD_users_DB = dict( (e["mila_email_username"], e) for e in LD_users_DB )
+    DD_users_LDAP = dict( (e["mila_email_username"], e) for e in LD_users_LDAP )
+    # Note that the whole point of testing is that we can express our success
+    # conditions in a clearer/different way than the implementation.
+    # If we just copy/pasted the cope from `client_side_user_updates` here
+    # then we would hardly be testing its logic.
+
+    for u in LD_users_updated:
+        
+        # Ask the question : Should u["status"] be "enabled"?
+        answer = True
+
+        # If it was present in the DB but wasn't enabled, the answer is False.
+        if u["mila_email_username"] in DD_users_DB:
+            if DD_users_DB[u["mila_email_username"]]["status"] != "enabled":
+                answer = False
+
+        # By construction we know that it's present in the LDAP, so now we check
+        # to make sure it's enabled there too.
+        answer = answer and (DD_users_LDAP[u["mila_email_username"]]["status"] == "enabled")
+
+        assert u["status"] == ("enabled" if answer else "disabled")
