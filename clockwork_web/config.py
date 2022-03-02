73f9fbd0a77598f2271724c208d3ccfaabda018a
Arnaud Bergeron <abergeron@gmail.com>
2022-03-02 12:20:21 -0500

    Cleanup and document config.py

--- clockwork_web/config.py
+++ clockwork_web/config.py
@@ -3,34 +3,34 @@ import copy
 
 import toml
 
-_config = None
-_config_file = os.environ.get("CLOCKWORK_CONFIG", None)
-
-_defaults = {}
-
+# This is a marker to indicate no default value
 _NoDefault = object()
 
-
-def _get_dict(d, key, create=False):
-    keys = key.split(".")
-    for k in keys[:-1]:
-        if create:
-            d = d.setdefault(k, {})
-        else:
-            d = d[k]
-    return d, keys[-1]
-
-
-def anything(value):
-    return value
+_config = None
+_defaults = {}
+_config_file = os.environ.get("CLOCKWORK_CONFIG", None)
 
 
 def register_config(key, default=_NoDefault, validator=anything):
+    """Register a configuration key.
+
+    The key should be in the <domain>.<name> format, but can have
+    multiple levels of dots.
+    """
     d, k = _get_dict(_defaults, key, create=True)
     d[k] = (default, validator)
 
 
 def get_config(key):
+    """Get the value for a configuration key.
+
+    The key must have been previously registered with register_config().
+
+    This also triggers the loading and parsing of the configuration
+    file when first called. Do not call it at the top level in a file
+    otherwise it will prevent other files from registering their
+    configuration keys.
+    """
     global _config
     if _config is None:
         _load_config()
@@ -44,12 +44,52 @@ def get_config(key):
     return value
 
 
+def _get_dict(d, key, create=False):
+    # Index into a multi-level dictionary using a dotted name.
+    #
+    # For d = {'a': {'b': 1, 'c': 2}} and the key 'a.b', this would return
+    # ({'b': 1, 'c': 2}, 'b')
+    #
+    # This is to support modification of the last level dictionary
+    #
+    # Note that it will create any missing intermediate dictionaries.
+    keys = key.split(".")
+    for k in keys[:-1]:
+        if create:
+            d = d.setdefault(k, {})
+        else:
+            d = d[k]
+    return d, keys[-1]
+
+
+# Placeholder for validation
+def anything(value):
+    return value
+
+
 # This should use the logger at the warning level
 def _warn(msg):
     print(msg)
 
 
+def _load_config():
+    # Load the configuration file, if any, and merge its content with the
+    # defaults to form the application configuration.
+    global _config
+    if _config_file is not None:
+        file_dict = toml.load(_config_file)
+        _config = _merge_configs(file_dict, _defaults)
+    else:
+        _config = _cleanup_defaults(_defaults)
+
+
 def _merge_configs(new_dict, default_dict):
+    # Produce a configuration dictionary from the defaults and configured values
+    #
+    # The return value will be a dictionary with the same structure as
+    # default_dict, but with values from new_dict, if present.
+    #
+    # Extra keys present in new_dict are ignored.
     res = {}
     for k, v in default_dict.items():
         nv = new_dict.get(k, _NoDefault)
@@ -67,16 +107,8 @@ def _merge_configs(new_dict, default_dict):
 
 
 def _cleanup_default(default_dict):
+    # Cleanup extra values in a default dict for use as configuration.
     return {
         k: _cleanup_default(v[0]) if isinstance(v, dict) else v[0]
         for k, v in default_dict
     }
-
-
-def _load_config():
-    global _config
-    if _config_file is not None:
-        file_dict = toml.load(_config_file)
-        _config = _merge_configs(file_dict, _defaults)
-    else:
-        _config = _cleanup_defaults(_defaults)
