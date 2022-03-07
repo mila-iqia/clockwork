c7f49d94900d8556a792588f1e057326ff7025e1
Arnaud Bergeron <abergeron@gmail.com>
2022-03-07 13:21:42 -0500

    bit more documentation

--- clockwork_web/config.py
+++ clockwork_web/config.py
@@ -6,8 +6,12 @@ import toml
 # This is a marker to indicate no default value
 _NoDefault = object()
 
+# format: {key1: value, key2: {key3: value, ...}, ...}
 _config = None
+
+# format: {key1: (value, validator), key2: {key3: (value, validator), ...}, ...}
 _defaults = {}
+
 _config_file = os.environ.get("CLOCKWORK_CONFIG", None)
 
 
@@ -21,6 +25,16 @@ def register_config(key, default=_NoDefault, validator=anything):
 
     The key should be in the <domain>.<name> format, but can have
     multiple levels of dots.
+
+    `default` is the default value for this configuration option.  It
+    can remain unset and this will cause an error on access if it is
+    not present in the configuration file (this may change to an error
+    on file load in the future).
+
+    `validator` if a function which will get the value read from the
+    configuration and should validate that it is an acceptable value
+    or transform it to one and return the final value. The returned
+    value will be what is reflected in the configuration.
     """
     d, k = _get_dict(_defaults, key, create=True)
     d[k] = (default, validator)
@@ -57,7 +71,8 @@ def _get_dict(d, key, create=False):
     #
     # This is to support modification of the last level dictionary
     #
-    # Note that it will create any missing intermediate dictionaries.
+    # Note that it will create any missing intermediate dictionaries
+    # if `create` is True.
     keys = key.split(".")
     for k in keys[:-1]:
         if create:
@@ -94,20 +109,25 @@ def _merge_configs(new_dict, default_dict):
     for k, v in default_dict.items():
         nv = new_dict.get(k, _NoDefault)
         if nv is _NoDefault:
+            # In this case we use the value from default_dict since
+            # the key is not in new_dict
             if isinstance(v, dict):
                 res[k] = _cleanup_default(v)
             else:
+                # v[0] is the default value
                 res[k] = v[0]
         else:
+            # And here we extract and validate the value from new_dict
             if isinstance(v, dict):
                 res[k] = _merge_configs(nv, v)
             else:
+                # v[1] is the validator
                 res[k] = v[1](nv)
     return res
 
 
 def _cleanup_default(default_dict):
-    # Cleanup extra values in a default dict for use as configuration.
+    # Remove validators in a default dict for use as configuration.
     return {
         k: _cleanup_default(v[0]) if isinstance(v, dict) else v[0]
         for k, v in default_dict
