1e72630c1b27f64614b0394a6b2154d0789efb44
Soline <soline_blanc@outlook.fr>
2022-04-22 10:42:28 -0400

    Clean the installation of the requirements of slurm_state

--- produce_fake_data.sh
+++ produce_fake_data.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -eu
 
-# Build the Docker images
-docker build -t clockwork_scripts scripts
-
 # Set the environnement
 . ./env.sh
 export CLOCKWORK_SLURM_REPORT_PATH="<insert_slurm_report_local_path>" # TODO
@@ -13,4 +10,3 @@ trap "docker-compose down && docker-compose rm -fv" EXIT
 
 # Run the Docker containers
 docker-compose run clockwork_scripts
-# docker-compose run mongodb
