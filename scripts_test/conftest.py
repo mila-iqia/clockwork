251f3c658f5997656efc128e5f616f098c67d098
Soline <soline_blanc@outlook.fr>
2022-05-17 10:21:39 -0400

    These fixtures are not used yet

--- scripts_test/conftest.py
+++ scripts_test/conftest.py
@@ -9,27 +9,3 @@ from scripts_test.config import register_config
 
 register_config("mongo.connection_string", "")
 register_config("mongo.database_name", "clockwork")
-
-
-@pytest.fixture
-def app():
-    """Create and configure a new app instance for each test."""
-    # create the app with common test config
-    app = create_app(extra_config={"TESTING": True, "LOGIN_DISABLED": True})
-
-    # create the database and load test data
-    with app.app_context():
-        init_db()
-
-    yield app
-
-    # You can close file descriptors here and do other cleanup.
-    #
-    # cleanup_function()
-
-
-@pytest.fixture
-def client(app):
-    """A test client for the app."""
-    with app.test_client() as client:
-        yield client
