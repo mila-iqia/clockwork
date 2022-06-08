3c72d316347c86c6ce53c283223d99e13902ce34
Soline <soline_blanc@outlook.fr>
2022-06-08 18:01:04 -0400

    Tests for the helper functions of user

--- clockwork_web/core/users_helper.py
+++ clockwork_web/core/users_helper.py
@@ -6,12 +6,11 @@ from clockwork_web.db import get_db
 
 # Dictionary referencing the expected type for each web setting entity
 # TODO: do we call it "WEB_SETTINGS_TYPES" or do we keep a lowercase syntax?
-web_settings_types = {"nbr_items_per_page": int, "dark_mode": boolean}
+web_settings_types = {"nbr_items_per_page": int, "dark_mode": bool}
 
 # default value for the user setting "nbr_items_per_page"
 # TODO: do we call it "DEFAULT_NBR_ITEMS_PER_PAGE" or do we keep a lowercase syntax?
 default_nbr_items_per_page = 40
-
 
 def set_web_setting(mila_email_username, setting_key, setting_value):
     """
@@ -64,7 +63,12 @@ def is_correct_type_for_web_setting(setting_key, setting_value):
     """
     # If the element has a registered expected type
     if setting_key in web_settings_types:
-        return isinstance(setting_value, web_settings_types[setting_key])
+        # The problem here is that isinstance(True,int) returns True
+        # Thus, we handle boolean differently than any other type
+        if type(setting_value) != bool:
+            return isinstance(setting_value, web_settings_types[setting_key])
+        else:
+            return web_settings_types[setting_key] == bool
     else:
         return False  # TODO: raise an error instead?
 
@@ -73,14 +77,23 @@ def set_items_per_page(mila_email_username, nbr_items_per_page):
     """
     Set the number of items to display by default for a specific user in a page
     presenting a list, such as a list of jobs, or a list of nodes.
+    If nbr_items_per_page is positive, it will be set as value to the setting
+    "nbr_items_per_page" of the user. If not, the default value is set.
 
     Parameters:
         mila_email_username     Element identifying the User in the users
                                 collection of the database
         nbr_items_per_page      New number of items to display, to set
     """
-    # Call set_web_setting
-    set_web_setting(mila_email_username, "nbr_items_per_page", nbr_items_per_page)
+    # If the value to set is an integer
+    if isinstance(nbr_items_per_page, int):
+        # Check if nbr_items_per_page is a positive number.
+        if (nbr_items_per_page <= 0):
+             # If not, set the default value for this parameter
+            reset_items_per_page(mila_email_username)
+        else:
+            # If it is positive, call set_web_setting
+            set_web_setting(mila_email_username, "nbr_items_per_page", nbr_items_per_page)
 
 
 def reset_items_per_page(mila_email_username):
@@ -111,7 +124,7 @@ def enable_dark_mode(mila_email_username):
     set_web_setting(mila_email_username, "dark_mode", True)
 
 
-def enable_dark_mode(mila_email_username):
+def disable_dark_mode(mila_email_username):
     """
     Disable the dark mode display option for a specific user.
 
