85d77ad18d336bf053fa316c3b3505807e5db552
Soline <soline_blanc@outlook.fr>
2022-06-22 11:52:16 -0400

    Add basic stub page for a user

---
+++ clockwork_web/browser_routes/users.py
@@ -0,0 +1,44 @@
+from flask import Blueprint, request, render_template
+from flask_login import(
+    current_user,
+    login_required
+)
+
+flask_api = Blueprint("users", __name__)
+
+import random # TODO: This is a temporary import
+
+from clockwork_web.core.users_helper import get_user
+
+@flask_api.route("/one")
+@login_required
+def route_one():
+    """
+    Takes "username" as a mandatory argument.
+
+    Returns a page displaying the User information if a username is
+    specified.
+    Returns an error message otherwise. The potential returned error
+    is:
+        - 400 ("Bad Request") if the username is missing.
+
+    .. :quickref: display the information of one user as formatted HTML
+    """
+    username = request.args.get("username", None)
+    if username is None:
+        return (
+            render_template("error.html", error_msg=f"Missing argument username."),
+            400,  # Bad Request
+        )
+
+    # Retrieve the user information
+    D_user = get_user(username)
+
+    return render_template(
+        "single_user.html",
+        # LP_user=LP_user,
+        username=username,
+        user=D_user,
+        mila_email_username=current_user.mila_email_username,
+        picture_name="user{}".format(random.randint(0,2)) # TODO: This is a temporary addition in order to user several images
+    )
