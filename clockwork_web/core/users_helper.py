8c3209b1aba445508039bc2bfe2856426724f8c4
Soline <soline_blanc@outlook.fr>
2022-06-22 11:52:50 -0400

    Black reformatting

--- clockwork_web/core/users_helper.py
+++ clockwork_web/core/users_helper.py
@@ -4,6 +4,7 @@ Helper functions in order to handle User information.
 
 from clockwork_web.db import get_db
 
+
 def get_user(username):
     """
     Get information of a specific user defined by its name.
@@ -23,8 +24,10 @@ def get_user(username):
         # as mila_email_username
         requested_mila_email_username = "{}@mila.quebec".format(username)
         found_user = db["users"].find_one(
-            {"mila_email_username": requested_mila_email_username}, # The filter we apply to find the user
-            {"_id": 0} # The '_id' element is not returned in the user's info
+            {
+                "mila_email_username": requested_mila_email_username
+            },  # The filter we apply to find the user
+            {"_id": 0},  # The '_id' element is not returned in the user's info
         )
         # Return the found user
         if found_user is not None:
