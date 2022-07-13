a3228ac4b5767a036c243a236abc193f32ec6e46
Soline <soline_blanc@outlook.fr>
2022-07-13 16:18:33 -0400

    Update the fields we want to set as optional

--- clockwork_web/core/clusters_helper.py
+++ clockwork_web/core/clusters_helper.py
@@ -35,17 +35,17 @@ def _load_clusters_from_config():
     # Define the format of a cluster
     clusters_valid = SubdictValidator({})
     # Global information
-    clusters_valid.add_field("organism", string)
+    clusters_valid.add_optional_field("organism", string)
     clusters_valid.add_field("timezone", timezone)
     # Allocations information
     clusters_valid.add_field("account_field", string)
     clusters_valid.add_field("allocations", alloc_valid)
     # Hardware information
-    clusters_valid.add_field("nbr_cpus", int)
-    clusters_valid.add_field("nbr_gpus", int)
+    clusters_valid.add_optional_field("nbr_cpus", int)
+    clusters_valid.add_optional_field("nbr_gpus", int)
     # Links to the documentation
-    clusters_valid.add_field("official_documentation", string)
-    clusters_valid.add_field("mila_documentation", optional_string)
+    clusters_valid.add_optional_field("official_documentation", string)
+    clusters_valid.add_optional_field("mila_documentation", optional_string)
 
     # Load the clusters from the configuration file, asserting that it uses the
     # predefined format
