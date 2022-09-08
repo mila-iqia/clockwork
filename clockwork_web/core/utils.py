399449da8cbe1bcf57be571466b9d4ab71ec82d3
Soline <soline_blanc@outlook.fr>
2022-09-08 15:07:55 -0400

    Split the expected array with the commas in the request of /jobs/search

--- clockwork_web/core/utils.py
+++ clockwork_web/core/utils.py
@@ -23,3 +23,27 @@ def to_boolean(arg):
         return True
     else:
         return False
+
+
+def get_custom_array_from_request_args(request_arg):
+    """
+    Create an array from a string by splitting it with its commas.
+    Thus, a string of the format "item1,item2,item3" as input parameter
+    would cause the following output: ["item1", "item2", "item3"].
+
+    Parameters:
+        - request_arg     The string to parse in order to get an array
+
+    Returns:
+        An array containing one or more strings.
+    """
+    # Initialise the array
+    returned_strings = []
+
+    # For each element of the splitted request_arg, add it to the array
+    for str_element in request_arg.split(","):
+        if str_element != "":
+            returned_strings.append(str_element)
+
+    # Return the array
+    return returned_strings
