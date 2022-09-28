4766c626da53815d8a5568b762d715fa390a503a
Soline <soline_blanc@outlook.fr>
2022-09-28 14:12:17 -0400

    Order the clusters when they are displayed

--- clockwork_web/core/clusters_helper.py
+++ clockwork_web/core/clusters_helper.py
@@ -46,6 +46,8 @@ def _load_clusters_from_config():
     # Links to the documentation
     clusters_valid.add_field("official_documentation", optional_string, default=False)
     clusters_valid.add_field("mila_documentation", optional_string, default=False)
+    # Priority indice used to know in which order we display the clusters
+    clusters_valid.add_field("display_priority", int, default=-1)
 
     # Load the clusters from the configuration file, asserting that it uses the
     # predefined format
