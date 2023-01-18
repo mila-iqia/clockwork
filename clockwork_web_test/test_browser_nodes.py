90b73e4916768340c29065967e51198a94c51cf3
Soline <soline_blanc@outlook.fr>
2023-01-18 14:14:37 -0500

    Update tests for displaying nodes

--- clockwork_web_test/test_browser_nodes.py
+++ clockwork_web_test/test_browser_nodes.py
@@ -26,6 +26,16 @@ def test_nodes(client, fake_data: dict[list[dict]]):
     Note that the `client` fixture depends on other fixtures that
     are going to put the fake data in the database for us.
     """
+    # Initialization
+    current_user_id = (
+        "student00@mila.quebec"  # student00 has access to the Mila and DRAC clusters
+    )
+
+    # Log in to Clockwork as this user
+    login_response = client.get(f"/login/testing?user_id={current_user_id}")
+    assert login_response.status_code == 302  # Redirect
+
+    # Request the nodes list
     response = client.get("/nodes/list")
 
     # Sort the nodes contained in the fake data by name, then cluster name
@@ -37,6 +47,10 @@ def test_nodes(client, fake_data: dict[list[dict]]):
     for i in range(0, get_default_setting_value("nbr_items_per_page")):
         D_node = sorted_all_nodes[i]
         assert D_node["slurm"]["name"] in response.get_data(as_text=True)
+
+    # Log out from Clockwork
+    response_logout = client.get("/login/logout")
+    assert response_logout.status_code == 302  # Redirect
 
 
 @pytest.mark.parametrize(
@@ -57,6 +71,15 @@ def test_nodes_with_both_pagination_options(
         page_num            The number of the page displaying the nodes
         nbr_items_per_page  The number of nodes we want to display per page
     """
+    # Initialization
+    current_user_id = (
+        "student00@mila.quebec"  # student00 has access to the Mila and DRAC clusters
+    )
+
+    # Log in to Clockwork as this user
+    login_response = client.get(f"/login/testing?user_id={current_user_id}")
+    assert login_response.status_code == 302  # Redirect
+
     # Get the response
     response = client.get(
         f"/nodes/list?page_num={page_num}&nbr_items_per_page={nbr_items_per_page}"
@@ -80,6 +103,10 @@ def test_nodes_with_both_pagination_options(
             D_node = sorted_all_nodes[i]
             assert D_node["slurm"]["name"] in response.get_data(as_text=True)
 
+    # Log out from Clockwork
+    response_logout = client.get("/login/logout")
+    assert response_logout.status_code == 302  # Redirect
+
 
 @pytest.mark.parametrize("page_num", [1, 2, 3, "lala", 7.8, False])
 def test_nodes_with_page_num_pagination_option(
@@ -97,6 +124,15 @@ def test_nodes_with_page_num_pagination_option(
         page_num            The number of the page displaying the nodes
         nbr_items_per_page  The number of nodes we want to display per page
     """
+    # Initialization
+    current_user_id = (
+        "student00@mila.quebec"  # student00 has access to the Mila and DRAC clusters
+    )
+
+    # Log in to Clockwork as this user
+    login_response = client.get(f"/login/testing?user_id={current_user_id}")
+    assert login_response.status_code == 302  # Redirect
+
     # Get the response
     response = client.get(f"/nodes/list?page_num={page_num}")
 
@@ -118,6 +154,10 @@ def test_nodes_with_page_num_pagination_option(
             D_node = sorted_all_nodes[i]
             assert D_node["slurm"]["name"] in response.get_data(as_text=True)
 
+    # Log out from Clockwork
+    response_logout = client.get("/login/logout")
+    assert response_logout.status_code == 302  # Redirect
+
 
 @pytest.mark.parametrize("nbr_items_per_page", [1, 29, 50, -1, [1, 2], True])
 def test_nodes_with_page_num_pagination_option(
@@ -134,6 +174,15 @@ def test_nodes_with_page_num_pagination_option(
         fake_data           The data our tests are based on
         nbr_items_per_page  The number of nodes we want to display per page
     """
+    # Initialization
+    current_user_id = (
+        "student00@mila.quebec"  # student00 has access to the Mila and DRAC clusters
+    )
+
+    # Log in to Clockwork as this user
+    login_response = client.get(f"/login/testing?user_id={current_user_id}")
+    assert login_response.status_code == 302  # Redirect
+
     # Get the response
     response = client.get(f"/nodes/list?nbr_items_per_page={nbr_items_per_page}")
 
@@ -155,6 +204,10 @@ def test_nodes_with_page_num_pagination_option(
             D_node = sorted_all_nodes[i]
             assert D_node["slurm"]["name"] in response.get_data(as_text=True)
 
+    # Log out from Clockwork
+    response_logout = client.get("/login/logout")
+    assert response_logout.status_code == 302  # Redirect
+
 
 @pytest.mark.parametrize(
     "cluster_name", ("mila", "cedar", "graham", "beluga", "sephiroth")
@@ -164,6 +217,14 @@ def test_nodes_with_filter(client, fake_data: dict[list[dict]], cluster_name):
     Same as `test_nodes` but adding a filter for the key "cluster_name".
     This tests the encoding of args with the ?k1=v1&k2=v2 notation in the url.
     """
+    # Initialization
+    current_user_id = (
+        "student00@mila.quebec"  # student00 has access to the Mila and DRAC clusters
+    )
+
+    # Log in to Clockwork as this user
+    login_response = client.get(f"/login/testing?user_id={current_user_id}")
+    assert login_response.status_code == 302  # Redirect
 
     # Yes, "sephiroth" is a fake cluster name. There are no entries in the data for it.
 
@@ -194,8 +255,22 @@ def test_nodes_with_filter(client, fake_data: dict[list[dict]], cluster_name):
             # with some integer.
             assert D_node["slurm"]["name"] not in response.get_data(as_text=True)
 
+    # Log out from Clockwork
+    response_logout = client.get("/login/logout")
+    assert response_logout.status_code == 302  # Redirect
+
 
 def test_single_node(client, fake_data):
+
+    # Initialization
+    current_user_id = (
+        "student00@mila.quebec"  # student00 has access to the Mila and DRAC clusters
+    )
+
+    # Log in to Clockwork as this user
+    login_response = client.get(f"/login/testing?user_id={current_user_id}")
+    assert login_response.status_code == 302  # Redirect
+
     node = fake_data["nodes"][0]["slurm"]
     response = client.get(
         f"/nodes/one?node_name={node['name']}&cluster_name={node['cluster_name']}"
@@ -203,12 +278,42 @@ def test_single_node(client, fake_data):
     assert response.status_code == 200
     assert node["alloc_tres"] in response.get_data(as_text=True)
 
+    # Log out from Clockwork
+    response_logout = client.get("/login/logout")
+    assert response_logout.status_code == 302  # Redirect
+
 
 def test_single_node_not_found(client):
+    # Initialization
+    current_user_id = (
+        "student00@mila.quebec"  # student00 has access to the Mila and DRAC clusters
+    )
+
+    # Log in to Clockwork as this user
+    login_response = client.get(f"/login/testing?user_id={current_user_id}")
+    assert login_response.status_code == 302  # Redirect
+
     response = client.get("/nodes/one?node_name=patate009")
     assert response.status_code == 400
 
+    # Log out from Clockwork
+    response_logout = client.get("/login/logout")
+    assert response_logout.status_code == 302  # Redirect
+
 
 def test_single_node_cluster_only(client):
+    # Initialization
+    current_user_id = (
+        "student00@mila.quebec"  # student00 has access to the Mila and DRAC clusters
+    )
+
+    # Log in to Clockwork as this user
+    login_response = client.get(f"/login/testing?user_id={current_user_id}")
+    assert login_response.status_code == 302  # Redirect
+
     response = client.get("/nodes/one?cluster_name=mila")
     assert response.status_code == 400
+
+    # Log out from Clockwork
+    response_logout = client.get("/login/logout")
+    assert response_logout.status_code == 302  # Redirect
