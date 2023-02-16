f4b0818253bd6608f9c6d23beef6d2961d7ca9f5
Bruno Carrez <bruno.carrez@mila.quebec>
2023-02-15 22:10:26 -0500

    logging web routes: nodes

--- clockwork_web/browser_routes/nodes.py
+++ clockwork_web/browser_routes/nodes.py
@@ -4,6 +4,7 @@ import os
 import json
 import requests
 import time
+import logging
 from collections import defaultdict
 
 
@@ -63,6 +64,7 @@ def route_list():
 
     .. :quickref: list all Slurm nodes as formatted html
     """
+    logging.info(f"clockwork browser route: /nodes/list - current_user={current_user.mila_email_username}")
 
     # Initialize the request arguments (it is further transferred to the HTML)
     previous_request_args = {}
@@ -145,6 +147,8 @@ def route_one():
 
     .. :quickref: list one Slurm node as formatted html
     """
+    logging.info(f"clockwork browser route: /nodes/one - current_user={current_user.mila_email_username}")
+
     # Initialize the request arguments (it is further transferred to the expected HTML)
     previous_request_args = {}
 
