e195124b507d71045e899a337a851e2bc3af9b0a
notoraptor <stevenbocco@gmail.com>
2023-06-09 13:58:22 -0400

    Fill status page with relevant info.

--- clockwork_web/browser_routes/status.py
+++ clockwork_web/browser_routes/status.py
@@ -8,9 +8,11 @@ from flask_login import current_user, login_required
 from flask_babel import gettext
 
 from clockwork_web.core.clusters_helper import get_all_clusters
-from clockwork_web.core.jobs_helper import get_jobs, get_filter_cluster_name
-from clockwork_web.core.nodes_helper import get_nodes
-from clockwork_web.core.users_helper import render_template_with_user_settings
+from clockwork_web.core.jobs_helper import get_jobs
+from clockwork_web.core.users_helper import (
+    render_template_with_user_settings,
+    get_users,
+)
 
 flask_api = Blueprint("status", __name__)
 
@@ -23,41 +25,39 @@ def route_status():
         f"clockwork_web route: /clusters/status  - current_user={current_user.mila_email_username}"
     )
 
-    # Get the clusters and reformat them in order to keep what we want.
+    users = get_users()
+
+    # Count users.
+    nb_users = len(users)
+
+    # Count enabled users.
+    nb_enabled_users = sum(
+        (1 for user in users if user["status"] == "enabled"), start=0
+    )
+
+    # Count users that have a DRAC account.
+    # User has a DRAC account if user dict contains a valid value for field "cc_account_username".
+    nb_drac_users = sum(
+        (1 for user in users if user.get("cc_account_username", None)), start=0
+    )
+
+    # Count number of jobs per cluster.
     D_all_clusters = get_all_clusters()
-    user_clusters = current_user.get_available_clusters()
-
     clusters = {}
-    # Keep only clusters available for current user.
-    for current_cluster_name in sorted(user_clusters):
-        dct = {}
-
-        # get job slurm updates.
+    for current_cluster_name in sorted(D_all_clusters):
         jobs, _ = get_jobs(cluster_names=[current_cluster_name])
-        job_dates = [
-            job["cw"]["last_slurm_update"]
-            for job in jobs
-            if "last_slurm_update" in job["cw"]
-        ]
-        # Get node slurm updates.
-        nodes, _ = get_nodes(get_filter_cluster_name(current_cluster_name))
-        node_dates = [
-            node["cw"]["last_slurm_update"]
-            for node in nodes
-            if "last_slurm_update" in node["cw"]
-        ]
+        clusters[current_cluster_name] = {"nb_jobs": len(jobs)}
 
-        # Save min and max dates for jobs and nodes.
-        if job_dates:
-            dct["job_dates"] = {"min": min(job_dates), "max": max(job_dates)}
-        if node_dates:
-            dct["node_dates"] = {"min": min(node_dates), "max": max(node_dates)}
-
-        clusters[current_cluster_name] = dct
+    server_status = {
+        "nb_users": nb_users,
+        "nb_enabled_users": nb_enabled_users,
+        "nb_drac_users": nb_drac_users,
+        "clusters": clusters or None,
+    }
 
     return render_template_with_user_settings(
         "status.html",
-        all_clusters=clusters,
+        server_status=server_status,
         mila_email_username=current_user.mila_email_username,
         previous_request_args={},
     )
